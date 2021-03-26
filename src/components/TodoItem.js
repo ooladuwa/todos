eff68c2d5014f557560971588872c5ecdd22a9d1
Onaje Oladuwa <32712596+ooladuwa@users.noreply.github.com>
2021-03-26 16:14:36 -0400

    Add files via upload

---
+++ src/components/TodoItem.js
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from "react";
+import styles from "./TodoItem.module.css";
+import { FaTrash } from "react-icons/fa";
+
+const TodoItem = (props) => {
+  const [editing, setEditing] = useState(false);
+
+  const handleEditing = () => {
+    setEditing(true);
+  };
+
+  const handleUpdatedDone = (event) => {
+    if (event.key === "Enter") {
+      setEditing(false);
+    }
+  };
+
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#595959",
+    opacity: 0.4,
+    textDecoration: "line-through",
+  };
+
+  const { completed, id, title } = props.todo;
+
+  let viewMode = {};
+  let editMode = {};
+
+  if (editing) {
+    viewMode.display = "none";
+  } else {
+    editMode.display = "none";
+  }
+
+  useEffect(() => {
+    return () => {
+      console.log("Cleaning up...");
+    };
+  }, []);
+
+  return (
+    <li className={styles.item}>
+      <div onDoubleClick={handleEditing} style={viewMode}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={completed}
+          onChange={() => props.handleChangeProps(id)}
+        />
+        <button onClick={() => props.deleteTodoProps(id)}>
+          <FaTrash
+            style={{
+              color: "darkcyan",
+              fontSize: "20px",
+              marginTop: "2px",
+            }}
+          />
+        </button>
+        <span style={completed ? completedStyle : null}>{title}</span>
+      </div>
+      <input
+        type="text"
+        style={editMode}
+        className={styles.textInput}
+        value={title}
+        onChange={(e) => {
+          props.setUpdate(e.target.value, id);
+        }}
+        onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  );
+};
+
+export default TodoItem;
+
+// class TodoItem extends React.Component {
+//   handleEditing = () => {
+//     this.setState({
+//       editing: true,
+//     });
+//   };
+//   state = {
+//     editing: false,
+//   };
+//   handleUpdateDone = (event) => {
+//     if (event.key === "Enter") {
+//       this.setState({ editing: false })
+//     }
+//   };
+//   render() {
+//     const completedStyle = {
+//       fontStyle: "italic",
+//       color: "#595959",
+//       opacity: 0.4,
+//       textDecoration: "line-through",
+//     };
+//     const { completed, id, title } = this.props.todo;
+
+//     let viewMode = {};
+//     let editMode = {};
+
+//     if (this.state.editing) {
+//       viewMode.display = "none";
+//     } else {
+//       editMode.display = "none";
+//     }
+//     return (
+//       <li className={styles.item}>
+//         <div onDoubleClick={this.handleEditing} style={viewMode}>
+//           <input
+//             type="checkbox"
+//             className={styles.checkbox}
+//             checked={completed}
+//             onChange={() => this.props.handleChangeProps(id)}
+//           />
+//           <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+//           <span style={completed ? completedStyle : null}>{title}</span>
+//         </div>
+//         <input
+//           type="text"
+//           style={editMode}
+//           className={styles.textInput}
+//           value={title}
+//           onChange={(e) => {
+//             this.props.setUpdate(e.target.value, id);
+//           }}
+//           onKeyDown={this.handleUpdateDone}
+//         />
+//       </li>
+//     );
+//   }
+// }
+// export default TodoItem;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// component file
+import TodoContainer from "./components/TodoContainer";
+import { BrowserRouter as Router } from "react-router-dom";
+
+// stylesheet
+import "./App.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <TodoContainer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
